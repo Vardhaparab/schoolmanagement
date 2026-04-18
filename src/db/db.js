eff1e309a8d59df6b37229926873aa30b247db61
Vardhaparab <vardhaparab06@gmail.com>
2026-04-18 15:41:52 +0530

    Fix repo and remove env

--- src/db/db.js
+++ src/db/db.js
@@ -5,7 +5,6 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT)
 });
 
 db.connect((err) => {
