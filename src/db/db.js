f99b4caaa14575beb169bbd68e7eec050f6de6b9
Vardhaparab <vardhaparab06@gmail.com>
2026-04-18 15:58:44 +0530

    fix mysql pool connection issue

--- src/db/db.js
+++ src/db/db.js
@@ -1,17 +1,22 @@
 const mysql = require ('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
     console.log("Database connection failed:", err.message);
   } else {
-    console.log("Database connected successfully");
+    console.log("Database connected successfully ");
+    connection.release();
   }
 });
 
